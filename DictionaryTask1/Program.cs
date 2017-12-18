c1e0af140dd8bc2b286b55e2d3ba232093f37541
delioff <delioff@gmail.com>
2017-12-19 00:30:43 +0200

    Added two tasks

---
+++ DictionaryTask1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> nums = Console.ReadLine().Split().Select(double.Parse).ToList();
+            SortedDictionary<double, int> count = new SortedDictionary<double, int>();
+            foreach (var num in nums)
+            {
+                if (count.ContainsKey(num))
+                {
+                    count[num] += 1;
+                }
+                else
+                {
+                    count.Add(num, 1);
+                }
+            }
+            foreach (var num in count)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+        }
+    }
+}
---
+++ DictionaryTask2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nums = Console.ReadLine().Split().ToList();
+            Dictionary<string, int> count = new Dictionary<string, int>();
+            foreach (var num in nums)
+            {
+                if (count.ContainsKey(num.ToLower()))
+                {
+                    count[num.ToLower()] += 1;
+                }
+                else
+                {
+                    count.Add(num.ToLower(), 1);
+                }
+            }
+            bool first = true;
+            foreach (var num in count)
+            {
+                if (num.Value % 2 != 0)
+                {
+                    if (first)
+                    {
+                        Console.Write($"{num.Key}");
+                        first = false;
+                    }
+                    else
+                    {
+                        Console.Write($", {num.Key}");
+                    }
+                }
+            }
+        }
+    }
+}
