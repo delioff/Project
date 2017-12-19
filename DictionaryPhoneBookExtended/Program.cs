349622c352315d8652903130ef0f81e8d38aaaac
atakov <atakov@DEV-4>
2017-12-19 11:29:10 +0200

    Added 2 new tasks

---
+++ DictionaryPhoneBookExtended/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPhonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool until = true;
+            SortedDictionary<string, string> phones = new SortedDictionary<string, string>();
+            while (until)
+            {
+                var input = Console.ReadLine().Split();
+                switch (input[0])
+                {
+                    case "A":
+                        if (phones.ContainsKey(input[1]))
+                        {
+                            phones[input[1]] = input[2];
+                        }
+                        else
+                        {
+                            phones.Add(input[1], input[2]);
+                        }
+                        break;
+                    case "S":
+                        if (phones.ContainsKey(input[1]))
+                        {
+                            Console.WriteLine($"{input[1]} -> {phones[input[1]]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Contact {input[1]} does not exist.");
+                        }
+                        break;
+                    case "ListAll":
+                        foreach (var phone in phones)
+                        {
+                            Console.WriteLine($"{phone.Key} -> {phone.Value}");
+                        }
+                        break;
+                    case "END": until = false; break;
+                }
+            }
+        }
+    }
+}
+
