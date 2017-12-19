8c8b78d54b4de5d390e4f0fe96190aeb2b3bf51b
atakov <atakov@DEV-4>
2017-12-19 14:31:22 +0200

    Adde more tasks

---
+++ DictionaryMinerTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryMiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool until = true;
+            Dictionary<string, long> resources = new Dictionary<string, long>();
+            while (until)
+            {
+                var res = Console.ReadLine();
+                if (res != "stop")
+                {
+                    var quantity = long.Parse(Console.ReadLine());
+                    if (resources.ContainsKey(res))
+                    {
+                        resources[res] += quantity;
+                    }
+                    else
+                    {
+                        resources.Add(res, quantity);
+                    }
+                }
+                else
+                {
+                    until = false;
+                }
+            }
+            foreach (var res in resources)
+            {
+                Console.WriteLine($"{res.Key} -> {res.Value}");
+            }
+        }
+    }
+}
